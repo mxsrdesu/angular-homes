0cf81ed97c23fe364345897b00be1df7b969a1d7
Maxim <h11d3nname@gmail.com>
2025-02-21 17:28:00 +0300

    completed app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello World!</h1>`,
+  template: `
+  <main>
+    <header header='brand-name'>
+      <img class='brand-logo' src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+    </header>
+    <section class='content'>
+      <router-outlet></router-outlet>
+    </section>
+  
+  </main>`
+  ,
   styleUrls: ['./app.component.css'],
+  imports: [RouterModule],
 })
 export class AppComponent {
   title = 'homes';
